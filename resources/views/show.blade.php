e7792511cfc2c6fd1804ae57f49d488dd92a2c02
Ru Tan <jutan2012@gmail.com>
2017-02-08 10:51:00 +0100

    Ajout balise form pour modifier le stock

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -6,12 +6,24 @@
 	<title>Show</title>
 </head>
 <body>
-	<h1>Product Manager</h1>
-	<h2>{{$fruit->name}}</h2>
-	<p>La couleur est {{$fruit->description}}.</p>
-	<p>Le prix est {{$fruit->prix /100}} €. </p>
-	<p>Il y a {{$fruit->stock}} .</p>
-	<p>Il est vient de {{$fruit->origine}} .</p>
-	<a href="/" class="ui button">Home</a>
+	<div class="ui container">
+		<h1>Product Manager</h1>
+		<h2>{{$fruit->name}}</h2>
+		<p>La couleur est {{$fruit->description}}.</p>
+		<p>Le prix est {{$fruit->prix /100}} €. </p>
+		<p>Il y a {{$fruit->stock}} .</p>
+		<div class="ui ">
+		<form action="/show/add/{{$fruit->id}}" method="post">
+		{{csrf_field()}} 
+			<button class="medium ui button"><i class="plus icon"></i></button>
+		</form> 
+		<form action="/show/down/{{$fruit->id}}" method="post">
+			{{csrf_field()}}
+			<button class="medium ui button"><i class="minus icon"></i></button>	
+		</form> 
+		</div>
+		<p>Il est vient de {{$fruit->origine}} .</p>
+		<a href="/" class="ui button">Home</a>
+	</div>
 </body>
 </html>
