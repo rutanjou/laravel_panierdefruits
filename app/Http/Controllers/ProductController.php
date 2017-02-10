fe45fdf43f27b4544653c5951eb5c8a6e6902216
Ru Tan <jutan2012@gmail.com>
2017-02-10 09:56:45 +0100

    modifier le route pour afficher function ajout

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -26,4 +26,36 @@ class ProductController extends Controller
      	$fruit->save();
      	return back();
      }
+     public function getAddFruit(){
+     	return view('/.addfruit');
+     }
+     public function postAddFruit(Request $request){
+    	$newFruit= new Fruit;
+    	$newFruit->name = $request->name;
+    	$newFruit->prix = $request->prix;
+    	$newFruit->stock = $request->stock;
+    	$newFruit->description = $request->description;
+    	$newFruit->origine = $request->origine;
+    	$newFruit->save();
+     	return redirect('/');
+     }
+     public function postDelete($id){
+     	$delete = Fruit::find($id);
+     	$delete->delete();
+     	return back();
+     }
+     public function getEdit($id){
+     		$editFruit = Fruit::find($id);
+     		return view('edit',['editFruit'=>$editFruit]);
+     }
+     public function postEditFruit($id, Request $request){
+     	$editFruit = Fruit::find($id);
+     	$editFruit->name = $request->name;
+     	$editFruit->prix = $request->prix;
+     	$editFruit->description = $request->description;
+     	$editFruit->origine = $request->origine;
+     	$editFruit->stock = $request->stock;
+     	$editFruit->save();
+     	return redirect('/');
+     }
 }
